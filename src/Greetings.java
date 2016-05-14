e6ea3cb6a0f8709a5ec2581ddf2e951e0b12bbb6
Sualeh Fatehi <sualeh@hotmail.com>
2016-05-13 22:27:44 -0400

    Initial code

---
+++ src/Greetings.java
@@ -0,0 +1,12 @@
+
+public class Greetings
+{
+  
+  public static void main(String[] args)
+  {
+    System.out.print("Hello ");
+    System.out.print("out ");
+    System.out.print("there");
+  }
+  
+}
---
+++ src/PrintingStringsAndNumbers.java
@@ -0,0 +1,11 @@
+
+public class PrintingStringsAndNumbers
+{
+  
+  public static void main(String[] args)
+  {
+    System.out.println("Stuff I want to say.");
+    System.out.println("Sch" + "wing"); // 
+  }
+  
+}
