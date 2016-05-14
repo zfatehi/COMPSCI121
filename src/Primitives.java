267855023b74880529258da7bcecd3d1bf106564
Sualeh Fatehi <sualeh@hotmail.com>
2016-05-13 23:02:01 -0400

    Primitives

---
+++ src/Primitives.java
@@ -0,0 +1,18 @@
+
+public class Primitives
+{
+
+  public static void main(String[] args)
+  {
+    int x = 1;
+
+    Integer y;
+    y = 1;
+    y = new Integer(1);
+
+    String name;
+    name = "Erik";
+    name = new String("Erik");
+  }
+
+}
