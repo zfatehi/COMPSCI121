e5d0e5afc4f3a01df65c51889fdc6e50589635c1
Sualeh Fatehi <sualeh@hotmail.com>
2016-05-13 22:38:06 -0400

    Comments and printing strings

--- src/Greetings.java
+++ src/Greetings.java
@@ -2,6 +2,7 @@
 public class Greetings
 {
   
+  // A static main runs a program, and is strictly not part of the class
   public static void main(String[] args)
   {
     System.out.print("Hello ");
--- src/PrintingStringsAndNumbers.java
+++ src/PrintingStringsAndNumbers.java
@@ -1,11 +1,39 @@
 
 public class PrintingStringsAndNumbers
 {
-  
+
   public static void main(String[] args)
   {
+    // Slide 1
+
+    // Print on a separate line
     System.out.println("Stuff I want to say.");
-    System.out.println("Sch" + "wing"); // 
+
+    // You can add strings together
+    System.out.println("Sch" + "wing");
+
+    // You can add numbers, and print the result
+    System.out.println(3 + 5);
+
+    // But, this is a string as a whole
+    System.out.println("3 + 5");
+
+    // Putting strings together
+    System.out.println("3" + "5");
+
+    // Slide 2
+
+    // Convert 5 to a string "5", first, if there is a string involved
+    System.out.println(5 + "a");
+
+    // Convert 5 to a string "5", first, if there is a string involved
+    System.out.println("a" + 5);
+
+    // Mixtures of strings and numbers
+    // • number + number --> number
+    // • string + string --> string
+    // • number + string --> string
+    // • string + number --> string
   }
-  
+
 }
